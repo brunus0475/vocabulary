22f6cbc432dd8e8cc2b6fbd706838cae7b5873b5
brunus0475 <43059446+brunus0475@users.noreply.github.com>
2024-01-08 16:42:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const config = require("../database/db.config.js");
 require("dotenv").config();
 const mongoose = require("mongoose");
-// console.log(config.database); // Outputs: 'mongodb://localhost:27017/VocabularyWebApp'
 
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
-// Establish the connection before starting the server
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
